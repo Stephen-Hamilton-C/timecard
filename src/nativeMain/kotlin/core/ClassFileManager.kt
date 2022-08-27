d3f75f7214057da32209331dc34f0442cabe4ba0
Stephen Hamilton <stephen.hamilton.c@gmail.com>
2022-08-27 16:04:14 -0400

    Documentation

--- src/nativeMain/kotlin/core/ClassFileManager.kt
+++ src/nativeMain/kotlin/core/ClassFileManager.kt
@@ -7,16 +7,33 @@ import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 
 // TODO: Need better name
+/**
+ * Standardizes how classes get loaded and saved to files.
+ * Create a new ClassFileManager with a VfsFile to where you expect the data for the class to be saved.
+ */
 class ClassFileManager(val file: VfsFile) {
+	/**
+	 * Saves the object to the file specified in the constructor.
+	 * @param obj An object that is a Serializable class.
+	 */
 	suspend inline fun <reified T> save(obj: T) {
+		// Create parent directories, then serialize to Json and save to file.
 		file.ensureParents()
 		file.writeString(Json.encodeToString(obj))
 	}
 	
+	/**
+	 * Creates an object from serialized data. Object must be a Serializable class.
+	 * @param defaultConstructor The default constructor for the object you wish to create.
+	 * Must either have no or all optional arguments.
+	 * Denoted with `::ClassName`.
+	 * This is used if there are problems reading or deserializing the file.
+	 */
 	suspend inline fun <reified T> load(defaultConstructor: () -> T): T {
+		// You know, this is one of very few times I'm happy functions are objects
 		if (!this.file.exists()) return defaultConstructor()
-		val data = this.file.readString()
 		return try {
+			val data = this.file.readString()
 			Json.decodeFromString(data)
 		} catch (e: Exception) {
 			val parentDir = this.file.parent
