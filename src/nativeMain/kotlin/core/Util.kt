68be1cc5f406f76b890765fffbad173af202d2e6
Stephen Hamilton <stephen.hamilton.c@gmail.com>
2022-08-31 17:05:38 -0400

    NOW is truncated to minutes Added nowMinus and todayMinus

--- src/nativeMain/kotlin/core/Util.kt
+++ src/nativeMain/kotlin/core/Util.kt
@@ -1,7 +1,11 @@
 package core
 
 import kotlinx.datetime.Clock
+import kotlinx.datetime.DateTimeUnit
+import kotlinx.datetime.LocalDate
+import kotlinx.datetime.LocalTime
 import kotlinx.datetime.TimeZone
+import kotlinx.datetime.minus
 import kotlinx.datetime.toLocalDateTime
 import kotlinx.datetime.todayIn
 
@@ -10,13 +14,33 @@ import kotlinx.datetime.todayIn
  */
 object Util {
 	/**
+	 * The current system time, with seconds and nanoseconds
+	 */
+	private val untrimmedNow: LocalTime = Clock.System.now()
+		.toLocalDateTime(TimeZone.currentSystemDefault()).time
+	
+	/**
 	 * The current system time.
 	 */
-	val NOW = Clock.System.now()
-		.toLocalDateTime(TimeZone.currentSystemDefault()).time
+	val NOW: LocalTime = LocalTime(untrimmedNow.hour, untrimmedNow.minute)
+	
+	/**
+	 * Gets the current system time, minus the given number of minutes.
+	 */
+	fun nowMinus(minutes: Int): LocalTime {
+		val untrimmedTime = Clock.System.now()
+			.minus(minutes, DateTimeUnit.MINUTE)
+			.toLocalDateTime(TimeZone.currentSystemDefault()).time
+		return LocalTime(untrimmedTime.hour, untrimmedTime.minute)
+	}
 	
 	/**
 	 * The current date, according to the system.
 	 */
-	val TODAY = Clock.System.todayIn(TimeZone.currentSystemDefault())
+	val TODAY: LocalDate = Clock.System.todayIn(TimeZone.currentSystemDefault())
+	
+	/**
+	 * The current date, minus the given number of days
+	 */
+	fun todayMinus(days: Int): LocalDate = TODAY.minus(days, DateTimeUnit.DAY)
 }
