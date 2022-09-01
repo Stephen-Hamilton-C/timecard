b2ecd8c10db0363d8f893a4c86f9db47bbfdb2f0
Stephen Hamilton <stephen.hamilton.c@gmail.com>
2022-09-01 17:10:10 -0400

    Added comment about caching

--- src/nativeMain/kotlin/config/Configuration.kt
+++ src/nativeMain/kotlin/config/Configuration.kt
@@ -15,6 +15,11 @@ data class Configuration(
 		private val _configDir = AppDirs.configUserDir("timecard", "Stephen-Hamilton-C")
 		
 		fun load(): Configuration = runBlocking {
+			// TODO: Unfortunately, I can't make a var in an object in Kotlin Native...
+			// but this is going to be costly if this isn't cached.
+			// May have to make an ugly workaround for this.
+			// Or maybe not. It is Native, which should be fast.
+			// If it needs optimization, I can do it later.
 			val manager = ClassFileManager(localVfs(_configDir)["config.json"])
 			return@runBlocking manager.load(::Configuration)
 		}
