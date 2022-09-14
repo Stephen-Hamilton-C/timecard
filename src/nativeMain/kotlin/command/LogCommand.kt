db55f764aac6561631d393948cf323fcb3b1b8a0
Stephen Hamilton <stephen.hamilton.c@gmail.com>
2022-09-14 10:29:35 -0400

    LogCommand can now show logs for other days

--- src/nativeMain/kotlin/command/LogCommand.kt
+++ src/nativeMain/kotlin/command/LogCommand.kt
@@ -2,8 +2,10 @@ package command
 
 import core.Color.green
 import core.Color.red
+import core.Color.yellow
 import core.TimeEntries
 import core.Util
+import kotlin.math.abs
 
 class LogCommand : ICommand {
 	override val name = "LOG"
@@ -12,11 +14,32 @@ class LogCommand : ICommand {
 	override val shortDescription: String = "Shows a log of entries for today. Providing days shows the log for that many days ago."
 	
 	override fun execute(args: List<String>) {
-		// TODO: Take in one argument to load a Timecard from NUM days ago
-		val timeEntries = TimeEntries.load()
+		val daysArg = args.getOrNull(1)
+		val days = if(daysArg == null) {
+			0
+		} else {
+			try {
+				abs(daysArg.toInt())
+			} catch(nfe: NumberFormatException) {
+				println(yellow("Must provide a whole number for days argument!"))
+				return
+			}
+		}
+		
+		val timeEntries = TimeEntries.load(Util.todayMinus(days))
 		if(timeEntries.entries.isEmpty()) {
-			println("No entries for today. Use 'timecard in' to clock in.")
+			when(days) {
+				0 -> println("No entries for today. Use 'timecard in' to clock in.")
+				1 -> println("No entries for yesterday.")
+				else -> println("No entries for $days days ago.")
+			}
 			return
+		}
+		
+		when(days) {
+			0 -> println("Entries for today:")
+			1 -> println("Entries for yesterday:")
+			else -> println("Entries for $days days ago:")
 		}
 		
 		// Print out every entry
