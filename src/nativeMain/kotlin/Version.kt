c7f028704ea6a5e4da2623d06b9ce4c20bd87a79
Stephen Hamilton <stephen.hamilton.c@gmail.com>
2022-09-19 09:46:44 -0400

    Updated Version.kt

--- src/nativeMain/kotlin/Version.kt
+++ src/nativeMain/kotlin/Version.kt
@@ -1,3 +1,6 @@
+// Version constants used by app and read by Gradle.
+// If lines are added or removed, ensure line numbers are correct in build.gradle's project.ext block.
+
 const val VERSION: String =
 "0.1.0-SNAPSHOT"
 const val DEBIAN_VERSION: String =
