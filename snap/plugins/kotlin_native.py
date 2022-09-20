4b0ab48e0995e9ad45c6fb2e179b2316ff4d3979
Stephen Hamilton <stephen.hamilton.c@gmail.com>
2022-09-20 18:03:21 -0400

    Working on custom snapcraft plugin

---
+++ snap/plugins/kotlin_native.py
@@ -0,0 +1,26 @@
+import snapcraft
+
+class KotlinNative(snapcraft.BasePlugin):
+    @classmethod
+    def schema(cls):
+        schema = super().schema()
+        
+        # Add a new property called "my-property"
+        schema['properties']['my-property'] = {
+                'type': 'string',
+        }
+
+        # The "my-option" property is now required
+        schema['required'].append('my-property')
+
+        return schema
+
+    def pull(self):
+        super().pull()
+        print('Pull done. Here is "my-property": {}'.format(
+            self.options.my_property))
+
+    def build(self):
+        super().build()
+        print('installDir: ' + super().installDir)
+        print('Build done.')
