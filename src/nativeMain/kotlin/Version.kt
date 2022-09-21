42caf62bf77d7909950842af0ae820ed7fabf2c9
Stephen Hamilton <stephen.hamilton.c@gmail.com>
2022-09-21 15:59:07 -0400

    Set version

--- src/nativeMain/kotlin/Version.kt
+++ src/nativeMain/kotlin/Version.kt
@@ -2,6 +2,6 @@
 // If lines are added or removed, ensure line numbers are correct in build.gradle's project.ext block.
 
 const val VERSION: String =
-"0.1.0-SNAPSHOT"
+"0.1.0"
 const val DEBIAN_VERSION: String =
 ""
