05420304eb3d7094023345ec487d9c3484ea8e3a
Stephen Hamilton <stephen.hamilton.c@gmail.com>
2022-09-21 11:04:52 -0400

    Snaps are now functional

--- snap/plugins/kotlin_native.py
+++ snap/plugins/kotlin_native.py
@@ -1,26 +1,29 @@
 import snapcraft
+import os
+import shutil
+import glob
 
-class KotlinNative(snapcraft.BasePlugin):
-    @classmethod
-    def schema(cls):
-        schema = super().schema()
-        
-        # Add a new property called "my-property"
-        schema['properties']['my-property'] = {
-                'type': 'string',
-        }
 
-        # The "my-option" property is now required
-        #schema['required'].append('my-property')
-
-        return schema
-
-    def pull(self):
-        super().pull()
-        print('Pull done. Here is "my-property": {}'.format(
-            self.options.my_property))
+class MyPlugin(snapcraft.BasePlugin):
 
     def build(self):
         super().build()
-        print('installDir: ' + super().installDir)
-        print('Build done.')
+
+        # Snapcraft API is so broken. I can't access any attributes of super().
+        # But vars can see them, so here I am, doing this awful hack.
+        superVars = vars(super())
+        partdir = superVars['partdir']
+        installdir = superVars['installdir']
+
+        # Build with Gradle
+        print("Building release executable...")
+        os.system("./gradlew linkReleaseExecutableNative")
+ 
+        # Get executable and strip its extension 
+        # because no one wants to type .kexe after the name of the command
+        artifactDir = "./build/bin/native/releaseExecutable"
+        executable = glob.glob(artifactDir + "/*.kexe")[0]
+        executableName = os.path.splitext(os.path.basename(executable))[0]
+
+        # Place build artifact into part staging area without .kexe extension
+        shutil.copy2(executable, os.path.join(installdir, executableName))
