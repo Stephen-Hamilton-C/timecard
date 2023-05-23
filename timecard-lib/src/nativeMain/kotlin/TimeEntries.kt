3f8badb2575ef2faa573032dbbf4b5de99a31501
Stephen-Hamilton-C <60950821+Stephen-Hamilton-C@users.noreply.github.com>
2023-05-23 15:41:46 -0400

    Remove platform specific TimeEntries for jvm and native

--- timecard-lib/src/nativeMain/kotlin/TimeEntries.kt
+++
@@ -1,9 +0,0 @@
-actual class TimeEntries {
-    actual var entries: List<TimeEntry>
-        get() = TODO("Not yet implemented")
-        set(value) {}
-
-    actual fun load(data: String) {
-    }
-
-}
